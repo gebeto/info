9eb81ab173e997e84f3ef0f28cc668d0f0053722
gebeto <slavik.nychkalo@gmail.com>
2019-12-13 13:03:36 +0200

    improve alpine init

--- configs/alpine-init.sh
+++ configs/alpine-init.sh
@@ -1,6 +1,8 @@
 
-apk add git curl gcc g++ make
+apk add git curl gcc g++ make zsh
 apk add openssh openrc
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
 
 # rc-update add sshd
 # /etc/init.d/sshd start
