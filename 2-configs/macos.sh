cfa37c36d81ed6b19bcc9fccd0a9f43ebaa55e19
gebeto <slavik.nychkalo@gmail.com>
2020-05-16 23:31:03 +0300

    Improved macos config

--- 2-configs/macos.sh
+++ 2-configs/macos.sh
@@ -8,5 +8,7 @@ killall Finder
 # Configure Dock
 defaults write com.apple.Dock autohide -bool yes
 defaults write com.apple.Dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.5
+defaults write com.apple.dock autohide-fullscreen-delayed -bool false
 
 killall Dock
