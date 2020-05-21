4d20aa14fd8d043d519ce1434392cbea2d5417df
gebeto <slavik.nychkalo@gmail.com>
2020-05-21 13:45:54 +0300

    Disabled dock autohide

--- 2-configs/macos.sh
+++ 2-configs/macos.sh
@@ -6,7 +6,7 @@ defaults write com.apple.finder _FXSortFoldersFirst -bool yes
 killall Finder
 
 # Configure Dock
-defaults write com.apple.Dock autohide -bool yes
+defaults write com.apple.Dock autohide -bool no
 defaults write com.apple.Dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.5
 defaults write com.apple.dock autohide-fullscreen-delayed -bool false
