a3c80668c5ac61fb940c3b8061f5163f22912e7b
gebeto <slavik.nychkalo@gmail.com>
2020-05-21 13:47:30 +0300

    Improved macos configuration

--- 2-configs/macos.sh
+++ 2-configs/macos.sh
@@ -1,11 +1,13 @@
 
 # Configure Finder
+echo "Finder configuration..."
 defaults write com.apple.finder AppleShowAllFiles -bool yes
 defaults write com.apple.finder _FXSortFoldersFirst -bool yes
 
 killall Finder
 
 # Configure Dock
+echo "Autohide dock..."
 defaults write com.apple.Dock autohide -bool no
 defaults write com.apple.Dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.5
